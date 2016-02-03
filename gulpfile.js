01e4a29dbcbb1ea9c378c7606e72e6f4f17ce197
julsmorozova <m.julie777@gmail.com>
2016-02-03 17:06:35 +0200

    edit gulpfile.js (create font, css tasks, add LiveReload, make embedded tasks)

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,49 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
+var livereload = require('gulp-livereload');
 var fileList = [
   './js/global.js', './js/student.js', './js/filter.js', './js/storage.js',
   './js/form-validator.js', './js/sort.js', './js/app.js'
 ];
-var compiledDest = './compiled/';
+var jsDest = './compiled/js';
+var cssDest = './compiled/css';
+var fontsDest = './compiled/fonts';
 
 gulp.task('compress', function() {
   return gulp.src(fileList)
     .pipe(concat('all.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(compiledDest))
-    .pipe(livereload());
+    .pipe(gulp.dest(jsDest));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('js/**/*.js', ['compress'])
+gulp.task('css', function() {
+  return gulp.src('sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(autoprefixer('last 2 version'))
+    // .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest(cssDest))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('fonts/**/*')
+  .pipe(gulp.dest(fontsDest));
+});
+
+// Watch
+gulp.task('watch', function() {
+  gulp.watch('sass/*.scss', ['css']);
+  gulp.watch('js/*.js', ['compress']);
+
+  // Create LiveReload server
+  livereload.listen();
+  gulp.watch(['compiled/**']).on('change', livereload.changed);
+
 });
